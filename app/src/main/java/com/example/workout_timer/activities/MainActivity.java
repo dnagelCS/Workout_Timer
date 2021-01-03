4df1a11e7f8d7e199af6806137ba395dd4e14bf6
Devora Nagel <devoranagel@gmail.com>
2021-01-03 00:07:35 -0600

    attempt at adding toggle items

--- app/src/main/java/com/example/workout_timer/activities/MainActivity.java
+++ app/src/main/java/com/example/workout_timer/activities/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.workout_timer.activities;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.example.workout_timer.R;
@@ -13,6 +14,7 @@ import static java.lang.String.format;
 
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -31,15 +33,82 @@ public class MainActivity extends AppCompatActivity {
     private TextView mTv_setNumRounds;
     private int rounds;
 
+    private SharedPreferences sharedPreferences;
+
+    private boolean mPrefBeepSound, mPrefTickSound;
+    private String mKeyBeepSound, mKeyTickSound;
+    private MenuItem beepSound = findViewById(R.id.beep_sound);
+    private MenuItem tickSound = findViewById(R.id.tick_sound);
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(mKeyBeepSound, mPrefBeepSound);
+        outState.putBoolean(mKeyTickSound, mPrefTickSound);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        saveToSharedPref();
+    }
+
+    private void saveToSharedPref() {
+        SharedPreferences.Editor editor = this.sharedPreferences.edit();
+        editor.clear();
+        editor.putBoolean(mKeyBeepSound, mPrefBeepSound);
+        editor.putBoolean(mKeyTickSound, mPrefTickSound);
+        editor.apply();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupToolbar();
+        setUpFAB();
+        setFieldsToResValues();
+        setUpDisplay();
+    }
+
+    private void setupToolbar() {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
 
-        setUpFAB();
-        setUpDisplay();
+    private void setUpFAB() {
+        ExtendedFloatingActionButton fab = findViewById(R.id.startButton);
+        fab.setOnClickListener(view -> {
+            //go to the TimerActivity now
+            Intent intent = new Intent(getApplicationContext(), TimerActivity.class);
+            intent.putExtra("ROUNDS", rounds);
+            intent.putExtra("REST_TIME", restTime);
+            intent.putExtra("WORK_TIME", workTime);
+            startActivity(intent);
+        });
+    }
+
+    private void setFieldsToResValues() {
+        mKeyBeepSound = getString(R.string.beep_sound);
+        mKeyTickSound = getString(R.string.tick_sound);
+    }
+
+    /**
+     * display that allows user to enter input
+     */
+    private void setUpDisplay() {
+        //mWorkTimer = new WorkTimer(DEFAULT_WORK_MILS, COUNTDOWN_TIMER_INTERVAL_MILS);
+        workTime = DEFAULT_WORK_MILS;
+        restTime = DEFAULT_REST_MILS;
+        rounds = DEFAULT_ROUNDS;
+
+        mTv_setWork = findViewById(R.id.workQuantity);
+        mTv_setRest = findViewById(R.id.restQuantity);
+        mTv_setNumRounds = findViewById(R.id.setsQuantity);
+
+        displayWorkTimerQuantity();
+        displayRestTimerQuantity();
+        displayRounds();
     }
 
     @Override
@@ -50,26 +119,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        switch (id) {
-            case R.id.action_settings:
-                showSettings();
-                return true;
-            case R.id.action_about:
-                showAbout();
-                return true;
+        if (id == R.id.beep_sound) {
+            toggleMenuItems();
+            mPrefBeepSound = item.isChecked();
+            return true;
+        } else if (id == R.id.tick_sound) {
+            toggleMenuItems();
+            mPrefTickSound = item.isChecked();
+            return true;
+        } else if (id == R.id.action_about) {
+            showAbout();
+            return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
-    private void showSettings() {
+    private void toggleMenuItems() {
+        beepSound.setChecked(!beepSound.isChecked());
+        tickSound.setChecked(!tickSound.isChecked());
     }
 
     private void showAbout() {
@@ -106,36 +177,6 @@ public class MainActivity extends AppCompatActivity {
         displayRestTimerQuantity();
     }
 
-    private void setUpFAB() {
-        ExtendedFloatingActionButton fab = findViewById(R.id.startButton);
-        fab.setOnClickListener(view -> {
-            //go to the TimerActivity now
-            Intent intent = new Intent(getApplicationContext(), TimerActivity.class);
-            intent.putExtra("ROUNDS", rounds);
-            intent.putExtra("REST_TIME", restTime);
-            intent.putExtra("WORK_TIME", workTime);
-            startActivity(intent);
-        });
-    }
-
-    /**
-     * display that allows user to enter input
-     */
-    private void setUpDisplay() {
-        //mWorkTimer = new WorkTimer(DEFAULT_WORK_MILS, COUNTDOWN_TIMER_INTERVAL_MILS);
-        workTime = DEFAULT_WORK_MILS;
-        restTime = DEFAULT_REST_MILS;
-        rounds = DEFAULT_ROUNDS;
-
-        mTv_setWork = findViewById(R.id.workQuantity);
-        mTv_setRest = findViewById(R.id.restQuantity);
-        mTv_setNumRounds = findViewById(R.id.setsQuantity);
-
-        displayWorkTimerQuantity();
-        displayRestTimerQuantity();
-        displayRounds();
-    }
-
     @SuppressLint("DefaultLocale")
     private void displayRounds() {
         mTv_setNumRounds.setText(format("%d", rounds));
@@ -146,7 +187,7 @@ public class MainActivity extends AppCompatActivity {
         int seconds = (int) restTime / 1000;
         int displayMinutes = seconds / 60;
         int displaySeconds = seconds % 60;
-      
+
         if (displaySeconds < 10) {
             mTv_setRest.setText(format("%d : 0%d", displayMinutes, displaySeconds));
         } else {
